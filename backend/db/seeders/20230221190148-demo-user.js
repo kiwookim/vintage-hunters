96e4b8b23c9d718a40509884f6be35c4e1e03687
Ki Woo Kim <kwedgemusic@gmail.com>
2023-02-21 11:05:28 -0800

    added users table migration model and seeds

---
+++ backend/db/seeders/20230221190148-demo-user.js
@@ -0,0 +1,46 @@
+"use strict";
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+	options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		options.tableName = "Users";
+		return queryInterface.bulkInsert(
+			options,
+			[
+				{
+					email: "demo@aa.io",
+					username: "Demo",
+					hashedPassword: bcrypt.hashSync("password"),
+				},
+				{
+					email: "marnie@aa.io",
+					username: "Marnie",
+					hashedPassword: bcrypt.hashSync("password"),
+				},
+				{
+					email: "bobbie@aa.io",
+					username: "Bobbie",
+					hashedPassword: bcrypt.hashSync("password"),
+				},
+			],
+			{}
+		);
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		options.tableName = "Users";
+		const Op = Sequelize.Op;
+		return queryInterface.bulkDelete(
+			options,
+			{
+				username: { [Op.in]: ["Demo", "Marnie", "Bobbie"] },
+			},
+			{}
+		);
+	},
+};
