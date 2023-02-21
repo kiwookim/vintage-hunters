38225cf89b3ac52ad5edba611397787264cb8bc2
Ki Woo Kim <kwedgemusic@gmail.com>
2023-02-21 15:40:45 -0800

    added user input validation on user signup endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,11 +1,34 @@
 const express = require("express");
-
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
 const router = express.Router();
 // Sign up
-router.post("/", async (req, res) => {
+const validateSignup = [
+	check("firstName")
+		.exists({ checkFalsy: true })
+		.withMessage("first name is required"),
+	check("lastName")
+		.exists({ checkFalsy: true })
+		.withMessage("last name is required"),
+	check("email")
+		.exists({ checkFalsy: true })
+		.isEmail()
+		.withMessage("Please provide a valid email."),
+	check("username")
+		.exists({ checkFalsy: true })
+		.isLength({ min: 4 })
+		.withMessage("Please provide a username with at least 4 characters."),
+	check("username").not().isEmail().withMessage("Username cannot be an email."),
+	check("password")
+		.exists({ checkFalsy: true })
+		.isLength({ min: 6 })
+		.withMessage("Password must be 6 characters or more."),
+	handleValidationErrors,
+];
+router.post("/", validateSignup, async (req, res) => {
 	const { email, password, username, firstName, lastName } = req.body;
 	const user = await User.signup({
 		email,
