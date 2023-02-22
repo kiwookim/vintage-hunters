1574165a026b69c7177aaa32435fcb1bdfc0870c
Ki Woo Kim <kwedgemusic@gmail.com>
2023-02-21 21:23:38 -0800

    loginpage component added

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,23 +1,26 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+
+import sessionReducer from "./session";
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
-  // add reducer functions here
+	// add reducer functions here
+	session: sessionReducer,
 });
 
 let enhancer;
 
 if (process.env.NODE_ENV === "production") {
-  enhancer = applyMiddleware(thunk);
+	enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require("redux-logger").default;
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+	const logger = require("redux-logger").default;
+	const composeEnhancers =
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+	enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
 const configureStore = (preloadedState) => {
-  return createStore(rootReducer, preloadedState, enhancer);
+	return createStore(rootReducer, preloadedState, enhancer);
 };
 
 export default configureStore;
