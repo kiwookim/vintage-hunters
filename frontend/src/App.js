2e8cbce5b425eda9de55f8a069a75304a6022552
Ki Woo Kim <kwedgemusic@gmail.com>
2023-02-21 22:22:04 -0800

    dropdown menu, navigation added, components added , thunk actions added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import SignUpFormPage from "./components/SignUpFormPage";
 import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
 	const dispatch = useDispatch();
@@ -13,13 +15,19 @@ function App() {
 	}, [dispatch]);
 
 	return (
-		isLoaded && (
-			<Switch>
-				<Route path='/login'>
-					<LoginFormPage />
-				</Route>
-			</Switch>
-		)
+		<>
+			<Navigation isLoaded={isLoaded} />
+			{isLoaded && (
+				<Switch>
+					<Route path='/login'>
+						<LoginFormPage />
+					</Route>
+					<Route path='/signup'>
+						<SignUpFormPage />
+					</Route>
+				</Switch>
+			)}
+		</>
 	);
 }
 
