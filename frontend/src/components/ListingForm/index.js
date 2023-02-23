4aa451362e5af69794757b7e8f91ec54e659fc32
Ki Woo Kim <kwedgemusic@gmail.com>
2023-02-23 06:38:49 -0800

    made choices.js for countries and categories list necessary for select field in the form

--- frontend/src/components/ListingForm/index.js
+++ frontend/src/components/ListingForm/index.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
-
+import { countries, categories } from "../../choices";
 export default function ListingForm() {
 	const [brandName, setBrandName] = useState("");
 	const [model, setModel] = useState("");
 	const [year, setYear] = useState("");
-	const [origin, setOrigin] = useState("");
+	const [originCountry, setOriginCountry] = useState("");
+	const [category, setCategory] = useState("");
+	const [listingTitle, setListingTitle] = useState("");
+	const [photoUrl, setPhotoUrl] = useState("");
+
 	return (
 		<section>
 			<h3>Tell us about your gear</h3>
@@ -39,6 +43,45 @@ export default function ListingForm() {
 						"1953-1957."
 					</p>
 				</div>
+				<label className='input-label-container'>
+					Manufacturer's Country <span className='required-tag'>REQUIRED</span>
+					<select
+						onChange={(e) => setOriginCountry(e.target.value)}
+						value={originCountry}
+					>
+						{countries.map((country) => (
+							<option key={country}>{country}</option>
+						))}
+					</select>
+				</label>
+				<label className='input-label-container'>
+					Categories <span className='required-tag'>REQUIRED</span>
+					<select
+						onChange={(e) => setCategory(e.target.value)}
+						value={category}
+					>
+						{categories.map((category) => (
+							<option key={category}>{category}</option>
+						))}
+					</select>
+				</label>
+				<label className='input-label-container'>
+					Listing Title <span className='required-tag'>REQUIRED</span>
+					<input
+						type='text'
+						value={listingTitle}
+						onChange={(e) => setListingTitle(e.target.value)}
+					/>
+				</label>
+				<label className='input-label-container'>
+					Upload Photo <span className='required-tag'>REQUIRED</span>
+					<input
+						type='text'
+						value={photoUrl}
+						onChange={(e) => setPhotoUrl(e.target.value)}
+						placeholder='put photo url here'
+					/>
+				</label>
 			</form>
 		</section>
 	);
