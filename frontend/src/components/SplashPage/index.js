67d1ffa7ae650e8106542b4bfcbf6d24861a0f9f
Ki Woo Kim <kwedgemusic@gmail.com>
2023-02-28 11:23:20 -0800

    splash page done

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -3,9 +3,44 @@ import "./SplashPage.css";
 export default function SplashPage() {
 	return (
 		<>
-			<h1>SPLASH PAGE</h1>
 			<div id='splash-bg'>
-				<h1>HELLO!</h1>
+				<div id='splash-txt'>
+					<h1>Find Your Dream Gear</h1>
+					<small>
+						Join small community of music lovers who use Vintage Hunters
+						<br /> to find your dream gear
+					</small>
+				</div>
+			</div>
+			<div id='splash-bottom-half'>
+				<h2>Anyone Can Sell On Vintage Hunters</h2>
+				<div className='seller-steps-container'>
+					<div className='step-container'>
+						<span>1</span>
+						<div className='step-txt'>
+							<h4>Make Shop Profile</h4>
+							<p>Easy to make your online shop</p>
+						</div>
+					</div>
+
+					<div className='step-container'>
+						<span>2</span>
+						<div className='step-txt'>
+							<h4>List It</h4>
+							<p>
+								Easy to List your gear and keep track of your other listings
+							</p>
+						</div>
+					</div>
+
+					<div className='step-container'>
+						<span>3</span>
+						<div className='step-txt'>
+							<h4>Sell It</h4>
+							<p>Gear heads like you search for gear on Vintage Hunters</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 	);
